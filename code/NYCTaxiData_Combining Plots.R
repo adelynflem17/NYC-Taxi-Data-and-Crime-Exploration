eb6cf400400e337df81cc2f172600ee2c0480c78
adelynflem17 <adelynflem17@gmail.com>
2025-10-21 21:57:09 -0500

    Update NYCTaxiData_Combining Plots.R
    Refactor code to streamline monthly taxi data processing and visualization. Updated the net movement calculation and improved the plotting function for better clarity and performance.

--- code/NYCTaxiData_Combining Plots.R
+++ code/NYCTaxiData_Combining Plots.R
@@ -1,5 +1,4 @@
-setwd("F:/ade/Desktop/USU Masters Work/Final MS Work/Datasets/Monthly Taxi Data/Cleaned Data 2")
-
+# Load required libraries
 library(dplyr)
 library(sf)
 library(readxl)
@@ -10,6 +9,7 @@ library(writexl)
 library(tidyverse)
 library(patchwork)
 
+# Load in final taxi-cab datasets for each month
 January_TaxiCab <- get(load("January_Final.csv"))
 February_TaxiCab <- get(load("February_Final.csv"))
 March_TaxiCab <- get(load("March_Final.csv"))
@@ -25,14 +25,13 @@ October_TaxiCab <- get(load("October_Final.csv"))
 November_TaxiCab <- get(load("November_Final.csv"))
 December_TaxiCab <- get(load("December_Final.csv"))
 
-
-setwd("F:/ade/Desktop/USU Masters Work/Final MS Work")
+# Read shapefiles and transform to WGS84
 fname <- unzip(zipfile = "taxi_zones.zip")
 zones <- sf::st_read("./taxi_zones.shp")
 zones <- sf::st_transform(zones, crs = 4326)
-nyc_boroughs <- st_read("F:/ade/Desktop/USU Masters Work/Final MS Work/R Code/nybb_24d/nybb.shp")
+nyc_boroughs <- st_read(".../nybb.shp")
 
-# Obtaining Spatial Outline for each individual Borough
+# Obtaining Spatial Outline for each Borough
 Queens <- subset(nyc_boroughs, nyc_boroughs$BoroName == "Queens")
 Brooklyn <- subset(nyc_boroughs, nyc_boroughs$BoroName == "Brooklyn")
 Bronx <- subset(nyc_boroughs, nyc_boroughs$BoroName == "Bronx")
@@ -49,21 +48,29 @@ StatenIsland_zones <- subset(zones, zones$borough == "Staten Island")
 
 
 # January
+
+# Calculate the number of individuals who moved out of each zone in January
 MoveOut_January <- subset(January_TaxiCab, January_TaxiCab$PULocationID != January_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Jan = sum(passenger_count)) %>%
   rename(., LocationID = PULocationID) %>%
   as.data.frame()
+
+# Calculate the number of individuals who moved into of each zone in January
 MoveIn_January <- subset(January_TaxiCab, January_TaxiCab$PULocationID != January_TaxiCab$DOLocationID) %>%
   group_by(DOLocationID) %>%
   summarise(Individuals_In_Jan = sum(passenger_count)) %>%
   rename(., LocationID = DOLocationID) %>%
   as.data.frame()
+
+# Merge inbound and outbound movement data by LocationID
 January_Month_Merged_Data <- merge(MoveIn_January, MoveOut_January, by = "LocationID")
+
+# Calculate net movement: positive = more arrivals than departures, negative = more departures
 January_Month_Merged_Data$Net_Movement <- January_Month_Merged_Data$Individuals_In_Jan - January_Month_Merged_Data$Individuals_Out_Jan
 
 
-# February
+# Calculating individuals who moved into and out of each zone in February
 MoveOut_February <- subset(February_TaxiCab, February_TaxiCab$PULocationID != February_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Feb = sum(passenger_count)) %>%
@@ -78,7 +85,7 @@ February_Month_Merged_Data <- merge(MoveIn_February, MoveOut_February, by = "Loc
 February_Month_Merged_Data$Net_Movement <- February_Month_Merged_Data$Individuals_In_Feb - February_Month_Merged_Data$Individuals_Out_Feb
 
 
-# March
+# Calculating individuals who moved into and out of each zone in March
 MoveOut_March <- subset(March_TaxiCab, March_TaxiCab$PULocationID != March_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_March = sum(passenger_count)) %>%
@@ -93,7 +100,7 @@ March_Month_Merged_Data <- merge(MoveIn_March, MoveOut_March, by = "LocationID")
 March_Month_Merged_Data$Net_Movement <- March_Month_Merged_Data$Individuals_In_March - March_Month_Merged_Data$Individuals_Out_March
 
 
-# April
+# Calculating individuals who moved into and out of each zone in April
 MoveOut_April <- subset(April_TaxiCab, April_TaxiCab$PULocationID != April_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_April = sum(passenger_count)) %>%
@@ -108,7 +115,7 @@ April_Month_Merged_Data <- merge(MoveIn_April, MoveOut_April, by = "LocationID")
 April_Month_Merged_Data$Net_Movement <- April_Month_Merged_Data$Individuals_In_April - April_Month_Merged_Data$Individuals_Out_April
 
 
-# May
+# Calculating individuals who moved into and out of each zone in May
 MoveOut_May <- subset(May_TaxiCab, May_TaxiCab$PULocationID != May_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_May = sum(passenger_count)) %>%
@@ -123,7 +130,7 @@ May_Month_Merged_Data <- merge(MoveIn_May, MoveOut_May, by = "LocationID")
 May_Month_Merged_Data$Net_Movement <- May_Month_Merged_Data$Individuals_In_May - May_Month_Merged_Data$Individuals_Out_May
 
 
-# June
+# Calculating individuals who moved into and out of each zone in June
 MoveOut_June <- subset(June_TaxiCab, June_TaxiCab$PULocationID != June_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_June = sum(passenger_count)) %>%
@@ -138,7 +145,7 @@ June_Month_Merged_Data <- merge(MoveIn_June, MoveOut_June, by = "LocationID")
 June_Month_Merged_Data$Net_Movement <- June_Month_Merged_Data$Individuals_In_June - June_Month_Merged_Data$Individuals_Out_June
 
 
-# July
+# Calculating individuals who moved into and out of each zone in July
 MoveOut_July <- subset(July_TaxiCab, July_TaxiCab$PULocationID != July_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_July = sum(passenger_count)) %>%
@@ -153,7 +160,7 @@ July_Month_Merged_Data <- merge(MoveIn_July, MoveOut_July, by = "LocationID")
 July_Month_Merged_Data$Net_Movement <- July_Month_Merged_Data$Individuals_In_July - July_Month_Merged_Data$Individuals_Out_July
 
 
-# August
+# Calculating individuals who moved into and out of each zone in August
 MoveOut_August <- subset(August_TaxiCab, August_TaxiCab$PULocationID != August_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Aug = sum(passenger_count)) %>%
@@ -168,7 +175,7 @@ August_Month_Merged_Data <- merge(MoveIn_August, MoveOut_August, by = "LocationI
 August_Month_Merged_Data$Net_Movement <- August_Month_Merged_Data$Individuals_In_Aug - August_Month_Merged_Data$Individuals_Out_Aug
 
 
-# September
+# Calculating individuals who moved into and out of each zone in September
 MoveOut_September <- subset(September_TaxiCab, September_TaxiCab$PULocationID != September_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Sept = sum(passenger_count)) %>%
@@ -183,7 +190,7 @@ September_Month_Merged_Data <- merge(MoveIn_September, MoveOut_September, by = "
 September_Month_Merged_Data$Net_Movement <- September_Month_Merged_Data$Individuals_In_Sept - September_Month_Merged_Data$Individuals_Out_Sept
 
 
-# October
+# Calculating individuals who moved into and out of each zone in October
 MoveOut_October <- subset(October_TaxiCab, October_TaxiCab$PULocationID != October_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Oct = sum(passenger_count)) %>%
@@ -198,7 +205,7 @@ October_Month_Merged_Data <- merge(MoveIn_October, MoveOut_October, by = "Locati
 October_Month_Merged_Data$Net_Movement <- October_Month_Merged_Data$Individuals_In_Oct - October_Month_Merged_Data$Individuals_Out_Oct
 
 
-# November
+# Calculating individuals who moved into and out of each zone in November
 MoveOut_November <- subset(November_TaxiCab, November_TaxiCab$PULocationID != November_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Nov = sum(passenger_count)) %>%
@@ -213,7 +220,7 @@ November_Month_Merged_Data <- merge(MoveIn_November, MoveOut_November, by = "Loc
 November_Month_Merged_Data$Net_Movement <- November_Month_Merged_Data$Individuals_In_Nov - November_Month_Merged_Data$Individuals_Out_Nov
 
 
-# December
+# Calculating individuals who moved into and out of each zone in December
 MoveOut_December <- subset(December_TaxiCab, December_TaxiCab$PULocationID != December_TaxiCab$DOLocationID) %>%
   group_by(PULocationID) %>%
   summarise(Individuals_Out_Dec = sum(passenger_count)) %>%
@@ -229,12 +236,10 @@ December_Month_Merged_Data$Net_Movement <- December_Month_Merged_Data$Individual
 
 
 
-
-# Change line 233 to whichever Borough zone we want to look at to create the heat maps and line 234 to whichever folder you want to save the visuals to
+# Set the borough you want to analyze
 Borough_Zone <- Brooklyn_zones
-setwd("F:/ade/Desktop/USU Masters Work/Final MS Work/Graphics/Final Crime Visuals/Movement by Zone/Net Movement 2")
 
-
+# Merge monthly movement data with spatial zone geometries 
 January_merged_data <- merge(Borough_Zone, January_Month_Merged_Data, by = "LocationID")
 February_merged_data <- merge(Borough_Zone, February_Month_Merged_Data, by = "LocationID")
 March_merged_data <- merge(Borough_Zone, March_Month_Merged_Data, by = "LocationID")
@@ -249,77 +254,14 @@ November_merged_data <- merge(Borough_Zone, November_Month_Merged_Data, by = "Lo
 December_merged_data <- merge(Borough_Zone, December_Month_Merged_Data, by = "LocationID")
 
 
-
-# Function to generate a plot for a given month
-#create_net_movement_plot <- function(data, borough, month_name, borough_name = "Queens", year = 2018) {
-#  Monthly_data_net <- data %>%
-#    mutate(rank = dense_rank(desc(Net_Movement))) %>%
-#    mutate(Net_Movement_Category = case_when(
-#      rank <= 5 ~ "red",
-#      rank <= 10 ~ "orange",
-#      Net_Movement > 0 & rank > 10 ~ "yellow",
-#      Net_Movement < 0 ~ "gray",
-#      TRUE ~ "gray"
-#    )) %>%
-#    mutate(Net_Movement_Category = factor(Net_Movement_Category, 
-#                                          levels = c("red", "orange", "yellow", "gray")))
-
-#  label_data <- Monthly_data_net %>%
-#    filter(Net_Movement_Category %in% c("red", "orange"))
-
-#  ggplot() +
-#    geom_sf(data = borough, fill = "white", show.legend = FALSE) + 
-#    geom_sf(data = Monthly_data_net, aes(geometry = geometry, fill = Net_Movement_Category),
-#            color = "black", size = 0.5) +
-#    geom_sf_label(data = label_data, aes(geometry = geometry, label = Net_Movement), 
-#                 fill = "white", color = "black", size = 1.0, label.size = 0.2,
-#                 show.legend = FALSE) +
-#    scale_fill_manual(name = "Net Movement Scale", 
-#                     values = c("red" = "red", "orange" = "orange", 
-#                                "yellow" = "yellow", "gray" = "gray80"),
-#                      labels = c("red" = "Top 5", "orange" = "Top 6-10", 
-#                                 "yellow" = "Positive Movement", "gray" = "Negative Movement"), drop = FALSE) +
-#    ggtitle(month_name) +
-#   theme_void() 
-#    theme(
-#      plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
-#      legend.position = "none")
-#}
-
-#month_list <- list(January = January_merged_data, February = February_merged_data, March = March_merged_data, 
-#                   April = April_merged_data, May = May_merged_data, June = June_merged_data, July = July_merged_data,
-#                  August = August_merged_data, September = September_merged_data, October = October_merged_data,
-#                   November = November_merged_data, December = December_merged_data)
-
-#plots <- map2(month_list, names(month_list), ~ create_net_movement_plot(.x, Borough_Zone, .y, borough_name = "Staten Island", year = 2018))
-
-#combined_plot <- wrap_plots(plots, ncol = 3, guides = "collect") &
-#  theme(legend.position = "bottom")
-
-#final_plot <- combined_plot +
-#  plot_annotation(
-#    title = "Queens 2018: Monthly Net Movement by Zone",
-#    theme = theme(
-#      plot.title = element_text(size = 14, face = "bold", hjust = 0.5)
-#    )
-#  )
-
-
-#ggsave("NetMovement_Combined_Queens.pdf", final_plot, width = 11, height = 8.5, units = "in", dpi = 300)
-
-
-
-
-
-
-
-
+# Function to create a net movement map for a given month
 create_net_movement_plot <- function(data, borough, month_name, borough_name = "Brooklyn", year = 2018) {
-  # Return empty plot if data is missing or empty
+  # Return an empty plot if data is missing or empty
   if (is.null(data) || nrow(data) == 0) {
     return(ggplot() + theme_void() + ggtitle(month_name))
   }
-  
+
+  # Rank zones by net movement and assign categories for coloring
   Monthly_data_net <- data %>%
     mutate(rank = dense_rank(desc(Net_Movement))) %>%
     mutate(Net_Movement_Category = case_when(
@@ -333,10 +275,12 @@ create_net_movement_plot <- function(data, borough, month_name, borough_name = "
     )) %>%
     mutate(Net_Movement_Category = factor(Net_Movement_Category, 
                                           levels = c("red", "orange", "yellow", "gray", "Zero", "Missing")))
-  
+
+  # Prepare label data for top zones
   label_data <- Monthly_data_net %>%
     filter(Net_Movement_Category %in% c("red", "orange"))
-  
+
+  # Build the map
   ggplot() +
     geom_sf(data = borough, fill = "white", show.legend = FALSE) + 
     geom_sf(data = Monthly_data_net, aes(geometry = geometry, fill = Net_Movement_Category),
@@ -362,7 +306,7 @@ create_net_movement_plot <- function(data, borough, month_name, borough_name = "
     )
 }
 
-# Create list of monthly data
+# Create a named list of monthly merged datasets
 month_list <- list(
   January = January_merged_data, February = February_merged_data, March = March_merged_data, 
   April = April_merged_data, May = May_merged_data, June = June_merged_data, July = July_merged_data,
@@ -370,14 +314,14 @@ month_list <- list(
   November = November_merged_data, December = December_merged_data
 )
 
-# Generate plots
+# Generate a list of plots for each month using map2
 plots <- map2(month_list, names(month_list), ~ create_net_movement_plot(.x, Borough_Zone, .y, borough_name = "Brooklyn", year = 2018))
 
-# Combine plots
+# Combine all monthly plots into a single layout
 combined_plot <- wrap_plots(plots, ncol = 3, guides = "collect") &
   theme(legend.position = "bottom")
 
-# Add annotation
+# Add a title annotation to the combined plot
 final_plot <- combined_plot +
   plot_annotation(
     title = "Brooklyn 2018: Monthly Net Movement by Zone",
@@ -386,8 +330,9 @@ final_plot <- combined_plot +
     )
   )
 
-# Save output
+# Save the final annotated plot to PDF
 ggsave("NetMovement_Combined_Brooklyn.pdf", final_plot, width = 11, height = 8.5, units = "in", dpi = 300)
+
 
 
 
